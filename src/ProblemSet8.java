91b33cc86a1c7c01a30bba8d0a78dd75e52e220c
Ryan <ryanagnello213@gmail.com>
2021-01-24 12:48:32 -0500

    finished

--- src/ProblemSet8.java
+++ src/ProblemSet8.java
@@ -1,14 +1,13 @@
 public class ProblemSet8 {
-
     public boolean common(int[] a, int[] b) {
-        if (a != null && b != null && a.length != 0 && b.length != 0) {
+        if (a != null && b != null && a.length != 0 && b.length != 0){
             return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
         }
         return false;
     }
 
     public int[] middleman(int[] a, int[] b) {
-        if (a != null && b != null && a.length % 2 == 1 && b.length % 2 == 1) {
+        if (a != null && b != null && a.length % 2 == 1 && b.length % 2 == 1){
             int aHalf = a.length/2;
             int bHalf = b.length/2;
             int[] ints = {a[aHalf], b[bHalf]};
@@ -20,7 +19,7 @@ public class ProblemSet8 {
     public int[] bigger(int[] a, int[] b) {
         int aSum = 0;
         int bSum = 0;
-        if (a != null && b != null) {
+        if (a != null && b != null){
             for(int i = 0; i < a.length; i++){
                 aSum += a[i];
             }
@@ -37,7 +36,7 @@ public class ProblemSet8 {
     }
 
     public int[] doubleMiddle(int[] a, int[] b) {
-        if(a != null && b != null && a.length % 2 == 0 && b.length % 2 == 0 && a.length > 0 && b.length > 0) {
+        if(a != null && b != null && a.length % 2 == 0 && b.length % 2 == 0 && a.length > 0 && b.length > 0){
             int[] doubles = {a[a.length/2-1], a[a.length/2], b[b.length/2-1], b[b.length/2]};
             return doubles;
         }
@@ -45,8 +44,8 @@ public class ProblemSet8 {
     }
 
     public int[] swapMe(int[] a) {
-        if(a != null) {
-            if (a.length >= 2) {
+        if(a != null){
+            if (a.length >= 2){
                 int first = a[0];
                 a[0] = a[a.length - 1];
                 a[a.length - 1] = first;
@@ -57,7 +56,7 @@ public class ProblemSet8 {
     }
 
     public int[] threePoint(int[] a) {
-        if (a != null && a.length % 2 == 1) {
+        if (a != null && a.length % 2 == 1){
             int[] prong = {a[0], a[a.length/2], a[a.length - 1]};
             return prong;
         }
@@ -66,8 +65,8 @@ public class ProblemSet8 {
 
     public int headHoncho(int[] a) {
         int max = -1;
-        if(a != null && a.length > 0) {
-            for(int i = 0; i < a.length; i++) {
+        if(a != null && a.length > 0){
+            for(int i = 0; i < a.length; i++){
                 if(a[i] < 0) {
                     return max;
                 }
@@ -92,10 +91,38 @@ public class ProblemSet8 {
     }
 
     public String halfway(int[] a) {
-
+        int leftHalf = 0;
+        int rightHalf = 0;
+        if(a != null && a.length % 2 == 0 && a.length > 0){
+            for(int i = 0; i <= a.length/2 - 1; i++){
+                leftHalf += a[i];
+            }
+            for(int j = a.length/2; j <= a.length - 1; j++){
+                rightHalf += a[j];
+            }
+            if(leftHalf > rightHalf){
+                return "LEFT";
+            } else {
+                return "RIGHT";
+            }
+        }
+        return null;
     }
 
     public int[] sequential(int[] a, int[] b) {
-
+        int[] three = new int[3];
+        if(a != null && b != null && a.length + b.length >= 3){
+            int x = 0;
+            for(int i = 0; i < 3; i++){
+                if(i < a.length){
+                    three[i] = a[i];
+                } else {
+                    three[i] = b[x];
+                    x++;
+                }
+            }
+            return three;
+        }
+        return null;
     }
 }
